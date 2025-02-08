197cdfa3bd12e1b34ad09707a3659cc5a4d80a7f
Andrzej <andrzej.konicki.programista@gmail.com>
2025-02-08 19:13:35 +0100

    First Init

---
+++ EnglishDictionary.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+word = input("Word:")
+print("----------------------------")
+
+url = 'https://dictionary.cambridge.org/dictionary/english-polish/' + word
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
+headers = {'User-Agent': user_agent}
+web_request = requests.get(url, headers=headers)
+soup = BeautifulSoup(web_request.text, "html.parser")
+TextToFile = "\n\n-----------\n" + word + "\n"
+print('MEANING:')
+if soup.find_all('div', class_='def ddef_d db'):
+    counter = 1
+    for ptag in soup.find_all('div', class_='def ddef_d db'):
+        if counter>5:
+            break
+        wynik = ptag.text
+        wynik = wynik.lstrip()
+        TextToFile = TextToFile + str(counter) +": " +wynik + "\n"
+        print(str(counter) +": " +wynik)
+        counter=counter+1
+else:
+    print("nothing found")
+TextToFile = TextToFile + "\n"        
+s = soup.find('div', attrs = {'class':'degs had lbt lb-cm'})
+try:
+    print("-----------")
+    print('EXAMPLES:')
+    counter = 1
+    for ptag in s.find_all('span', class_='deg'):
+        if counter>5:
+            break
+        wynik = ptag.text
+        wynik = wynik.lstrip()
+        wynik = wynik.rstrip()
+        TextToFile = TextToFile + str(counter) +": " +wynik + "\n"
+        print(str(counter) +": " +wynik)
+        counter=counter+1
+except AttributeError as e:
+    print("none")
+
+if soup.find_all('div', class_='def ddef_d db'):    
+    f = open('Printout.txt', 'a') 
+    f.write(TextToFile)
+    
+
